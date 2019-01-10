cd81e654c4adda158fbd804e9aa4a7d9341ef66d
Lucas-Brown <33075651+Lucas-Brown@users.noreply.github.com>
2019-01-09 18:20:13 -0700

    OI setup
    created all driver and operator remote buttons and axis

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,36 +7,49 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
+  XboxController driverController = new XboxController(RobotMap.DRIVER_CONTROLLER);
+	XboxController operatorController = new XboxController(RobotMap.OPERATOR_CONTROLLER);
+	
+	Button dButtonA = new JoystickButton(this.driverController, RobotMap.BUTTON_A);
+	Button dButtonB = new JoystickButton(this.driverController, RobotMap.BUTTON_B);
+	Button dButtonX = new JoystickButton(this.driverController, RobotMap.BUTTON_X);
+	Button dButtonY = new JoystickButton(this.driverController, RobotMap.BUTTON_Y);
+	Button dStartButton = new JoystickButton(this.driverController, RobotMap.START_BUTTON);
+	
+	Button oButtonA = new JoystickButton(this.operatorController, RobotMap.BUTTON_A);
+	Button oButtonB = new JoystickButton(this.operatorController, RobotMap.BUTTON_B);
+	Button oButtonY = new JoystickButton(this.operatorController, RobotMap.BUTTON_Y);
+	Button oButtonX = new JoystickButton(this.operatorController, RobotMap.BUTTON_X);
+	Button oButtonBack = new JoystickButton(this.operatorController, RobotMap.BACK_BUTTON);
+	Button oButtonStart = new JoystickButton(this.operatorController, RobotMap.START_BUTTON);
+  Button oButtonRightStick = new JoystickButton(this.operatorController, RobotMap.RIGHT_STICK_BUTTON);
+  
+  public boolean getOperatorButton(int axis) {
+		return this.operatorController.getRawButtonPressed(axis);
+	}
+	
+	public boolean getDriverButton(int axis) {
+		return this.driverController.getRawButtonPressed(axis);
+	}
+	
+	public double getOperatorRawAxis(int axis) {
+		return this.operatorController.getRawAxis(axis);
+	}
+	
+	public double getDriverRawAxis(int axis) {
+		return this.driverController.getRawAxis(axis);
+	}
 
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
-
-  //// TRIGGERING COMMANDS WITH BUTTONS
-  // Once you have a button, it's trivial to bind it to a button in one of
-  // three ways:
-
-  // Start the command when the button is pressed and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenPressed(new ExampleCommand());
-
-  // Run the command while the button is being held down and interrupt it once
-  // the button is released.
-  // button.whileHeld(new ExampleCommand());
-
-  // Start the command when the button is released and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenReleased(new ExampleCommand());
+	public OI () {
+		//this.oButtonRightStick.whenPressed(new WristControls());
+	}
 }
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -14,16 +14,41 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
-  
+
+  //remote values 
+	public static final int DRIVER_CONTROLLER = 0;
+	public static final int OPERATOR_CONTROLLER = 1;
+
+	public static final int BUTTON_A = 1;
+	public static final int BUTTON_B = 2;
+	public static final int BUTTON_X = 3;
+	public static final int BUTTON_Y = 4;
+	public static final int LEFT_BUMPER = 5;
+	public static final int RIGHT_BUMPER = 6;
+	public static final int BACK_BUTTON = 7;
+	public static final int START_BUTTON = 8;
+	public static final int LEFT_STICK_BUTTON = 9;
+	public static final int RIGHT_STICK_BUTTON = 10;
+	
+	public static final int LEFT_STICK_X = 0;
+	public static final int LEFT_STICK_Y = 1;
+	public static final int RIGHT_STICK_Y = 5;
+	public static final int RIGHT_STICK_X = 4;
+	
+	public static final int LEFT_TRIGGER = 2;
+	public static final int RIGHT_TRIGGER = 3;
+  //end of remote values
+
+  //motors
   public static final int LEFT_MOTOR_1 = 0; //motor PDP values
   public static final int LEFT_MOTOR_2 = 1;
   public static final int RIGHT_MOTOR_1 = 2;
   public static final int RIGHT_MOTOR_2 = 3;
 
-  public static final double RAMP_RATE = 0; //time in seconds for the motor to reach max speed
 
   //driver limitations and values
   public static final double MAX_SPEED = 0.95; // 0-1, max speed
+  public static final double RAMP_RATE = 0; //time in seconds for the motor to reach max speed
   public static final int CURRENT_LIMIT = 27;
 
 }
