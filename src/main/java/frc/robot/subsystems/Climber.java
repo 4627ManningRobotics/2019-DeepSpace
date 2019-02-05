f03dd80ccd4a81aa316e574c78278831b7469429
Joe Wilson <jtwilson.home@gmail.com>
2019-02-04 20:06:31 -0700

    fixed enum implementation

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -23,12 +23,12 @@ import frc.robot.commands.OperatorControls;
 public class Climber extends Subsystem {
 
   private final CANSparkMax front_climber = new CANSparkMax(RobotMap.FRONT_CLIMBER, MotorType.kBrushless); // drive train motors
-  private final CANSparkMax back_climber = new CANSparkMax(RobotMap.BACK_CLIMBER, MotorType.kBrushless);/*
-  private final DigitalInput FRONT_MAX = new DigitalInput(RobotMap.DIO.CLIMBER_FRONT_MAX);
-  private final DigitalInput FRONT_MIN = new DigitalInput();
-  private final DigitalInput BACK_MAX = new DigitalInput();
-  private final DigitalInput BACK_MIN = new DigitalInput();
-  */
+  private final CANSparkMax back_climber = new CANSparkMax(RobotMap.BACK_CLIMBER, MotorType.kBrushless);
+  private final DigitalInput FRONT_MAX = new DigitalInput(RobotMap.DIO.CLIMBER_FRONT_MAX.ordinal());
+  private final DigitalInput FRONT_MIN = new DigitalInput(RobotMap.DIO.CLIMBER_FRONT_MIN.ordinal());
+  private final DigitalInput BACK_MAX = new DigitalInput(RobotMap.DIO.CLIMBER_BACK_MAX.ordinal());
+  private final DigitalInput BACK_MIN = new DigitalInput(RobotMap.DIO.CLIMBER_BACK_MIN.ordinal());
+  
   public Climber(){
     this.front_climber.getPIDController().setP(RobotMap.FRONT_CLIMBER_P);
     this.front_climber.getPIDController().setI(RobotMap.FRONT_CLIMBER_I);
