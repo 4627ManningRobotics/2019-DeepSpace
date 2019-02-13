c4c401e65adff406be02d7d83844356fed736340
Lucas-Brown <33075651+Lucas-Brown@users.noreply.github.com>
2019-02-13 12:07:06 -0700

    added zeroing both climbers

---
+++ src/main/java/frc/robot/commands/ZeroBothClimbers.java
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.subsystems.Climber.Dart;
+
+public class ZeroBothClimbers extends CommandGroup {
+  /**
+   * Add your docs here.
+   */
+  public ZeroBothClimbers() {
+    addParallel(new ZeroClimber(Dart.FRONT));
+    addParallel(new ZeroClimber(Dart.BACK));
+    // Add Commands here:
+    // e.g. addSequential(new Command1());
+    // addSequential(new Command2());
+    // these will run in order.
+
+    // To run multiple commands at the same time,
+    // use addParallel()
+    // e.g. addParallel(new Command1());
+    // addSequential(new Command2());
+    // Command1 and Command2 will run in parallel.
+
+    // A command group will require all of the subsystems that each member
+    // would require.
+    // e.g. if Command1 requires chassis, and Command2 requires arm,
+    // a CommandGroup containing them would require both the chassis and the
+    // arm.
+  }
+}
